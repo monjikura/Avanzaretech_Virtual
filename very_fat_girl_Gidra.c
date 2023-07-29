327c99b18730acceffed17e0f43b6310a892e923
monjikura <68017784+monjikura@users.noreply.github.com>
2023-07-29 20:17:52 +0800

    after unit_test
    Please clean your $SHIT

---
+++ very_fat_girl_Gidra.c
@@ -0,0 +1,28 @@
+
+ # binary heap re_alloc with qword pointer
+
+ void _start(void) // offset 0x40 Snowman
+
+ {
+
+   _start_main();
+   return;
+
+ }   // entry call for very_fat_girl
+
+ code: *UNRECOVERED_JUMPTABLE;
+
+ /* WARNING: Could not recover jumptable at
+    0x0010179c. Too many branches
+    WARNING: Treating indirect jump as call */
+
+ void FUN_00101910(void)
+
+ {
+
+   (*(code *)(undefined *)0x0);
+   return;
+
+ }  // iVar1 detected
+
+
