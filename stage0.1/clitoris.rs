d37ebf5804a5d94189f81ab3f307afcff7c48c6c
monjikura <68017784+monjikura@users.noreply.github.com>
2023-08-20 00:29:26 +0800

    Easy paths (#2)
    * Create girl.toml
    
    * Add files via upload
    
    * Delete girl.toml
    
    * Create .g
    
    * yaml-json like wire cage
    
    * Delete .g
    
    * Update clitoris.rs
    
    * Delete clitoris.rs
    
    * Create .g
    
    * after 'traits' dessert for 'girl'
    
    * Delete .g
    
    * Create .g
    
    * Add files via upload
    
    * Delete .g
    
    * Create .g
    
    * Create .g
    
    * Delete .g
    
    * Delete .g
    
    * Create .g
    
    * Create polygammaf.cfg.toml
    
    * Delete .g
    
    * Create .g
    
    * Create badge.svg
    
    * Delete .g
    
    * Update badge.svg
    
    * Update badge.svg
    
    * Create .g
    
    * Create .g
    
    * Create white_noise
    
    * Create pink_noise
    
    * Delete .g
    
    * Create .g
    
    * Delete .g
    
    * Create .g
    
    * Create girl_toilet.xml
    
    * Delete .g
    
    * Create index.json
    
    * Delete .g
    
    * Add files via upload
    
    * Rename script to script.000004
    
    * Create notes
    
    * Create README
    
    * Delete README
    
    * Add files via upload
    
    * Update README
    
    * Update README
    
    * Update README
    
    * Update README
    
    * encrypted
    
    * Update 014___F-01.uint13-u13.json
    
    * Add files via upload
    
    * Update 768
    
    Maybsnk 1337
    
    * Update README
    
    * Add files via upload

---
+++ stage0.1/clitoris.rs
@@ -0,0 +1,16 @@
+
+ /* (the) impl keyword is primarily used to
+    define implementations on types. Inherent
+    implementations are standalone, while
+    trait implementations are used to implement
+    traits for types, or other traits.
+    Functions and consts can both be defined
+    in an implementation. */
+
+ pub struct("clitoris.rs"); 
+
+ // ...go to sleep ' || ' in Rust...?...??...
+
+ | println! | > ("'..girl...taxi..taxi', ' '");
+
+ // closure.js syntax - 'the' vs 'that'?   
