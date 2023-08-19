eaeb678813937ea9b898a2d29d21d892a6676c64
monjikura <68017784+monjikura@users.noreply.github.com>
2023-08-19 17:01:20 +0800

    Add files via upload

---
+++ ifne_girl_drive/PINK.py
@@ -0,0 +1,16 @@
+
+import pants
+import math
+import panties
+
+soiled_pantie01 = None
+random.randint()
+# no .<digit> floating literal anymore;
+# put 0 before dot
+pass.0.randint.128 while
+salmon = P* delta.company(-106, 166)
+# .128
+soiled_pantie01.append( )
+# python state machine verified pantie is DIRTY
+
+print ("...do not clean ")
