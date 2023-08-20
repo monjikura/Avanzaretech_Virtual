70cfe35ce455e5ce461d4d64abc75898e17fcd4e
monjikura <68017784+monjikura@users.noreply.github.com>
2023-08-20 21:06:52 +0800

    Add files via upload

---
+++ Blind return oriented programming/ladder_49.rs
@@ -0,0 +1,17 @@
+
+ perl ladder_49.rs
+ Old package separator "'" deprecated line %
+ Bareword found where operator expected
+ (Missing operator before "Mirzhakani's"?) at
+ ?line %,??... near:: ;
+ Execution of ladder_49.rs aborted due to
+ compilation errors
+
+ // Mirzhakani's 1st macro's'
+ reverse__octal::Notwendigkeit! 
+
+ $zeitnacht!::r#const::(o128)::[Bestimmter, Weise] 
+
+ // zu unternehmen glir zu erreichen
+
+ __Cargo
